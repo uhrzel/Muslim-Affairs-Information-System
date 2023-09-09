21e488619c420b52316a8c52c7547706ac347eec
aynjel <ortegacanillo76@gmail.com>
2023-09-09 21:00:25 +0800

    set up multi auth

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Models\User;
+
+use Illuminate\Support\Facades\Auth;
+
+class HomeController extends Controller
+{
+    /**
+     * Show the application dashboard.
+     *
+     * @param \Illuminate\Http\Request $request
+     *
+     * @return \Illuminate\Contracts\Support\Renderable
+     */
+    public function index(Request $request)
+    {
+        $user = Auth::user();
+
+        if ($user->type == 'admin') {
+            return view('admin.dashboard');
+        } elseif ($user->type == 'user') {
+            return view('user.dashboard');
+        } else {
+            return view('welcome');
+        }
+    }
+
+    public function post()
+    {
+        return view('post');
+    }
+}
